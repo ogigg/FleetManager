4540bd7983fc1d5eb9802a64957a1aeb1ce18290
ogigg <gierszon10@gmail.com>
2019-03-15 11:39:08 +0100

    Added Google Maps and Material

--- FleetManager/ClientApp/src/app/app.component.ts
+++ FleetManager/ClientApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { MapsService } from './maps.service';
 import { Component } from '@angular/core';
+import 'hammerjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Fleet Manager';
+  mapType: String ="roadmap";
+  lat: Number;
+  lng: Number;
+  
+  lat2: Number;
+  lng2: Number;
+  location: Object ="";
+
+  constructor(private map: MapsService) {
+
+  }
+  onSatelite(){
+    this.mapType="satellite"
+    console.log(this.mapType)
+  }
+  onRoadMap(){
+    this.mapType="roadmap"
+    console.log(this.mapType)
+  }
+  onHybrid(){
+    this.mapType="hybrid"
+    console.log(this.mapType)
+  }
+  ngOnInit(){
+    this.lat=52.4173;
+    this.lng=16.9647;
+    this.lat2=52.423;
+    this.lng2=16.9547;
+    console.log(this.lat,this.lng);
+  }
 }
--- FleetManager/ClientApp/src/app/app.module.ts
+++ FleetManager/ClientApp/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import { AgmCoreModule } from '@agm/core'
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,14 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAYbMdyEd-ppwZx9dU9o-iBrXqa37MtwvU'
+    }),
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
